87256ce95dcd5440beafcb28d626f94218c0ce96
Justin Wash <justin.wash@pm.me>
2019-02-07 15:53:03 -0600

    icon styling fix

--- src/app/rankings/rankings.component.ts
+++ src/app/rankings/rankings.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-rankings',
-  templateUrl: './rankings.component.html',
-  styleUrls: ['./rankings.component.scss']
+	selector: 'app-rankings',
+	templateUrl: './rankings.component.html',
+	styleUrls: ['./rankings.component.scss']
 })
 export class RankingsComponent implements OnInit {
+	constructor(public router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
