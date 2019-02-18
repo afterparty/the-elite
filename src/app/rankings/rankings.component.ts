650ccba22f64cdc37dc7fa7ce0935355b0b95d75
justin wash <justin.wash@pm.me>
2019-02-18 11:31:30 -0600

    add video links, fix run list

--- src/app/rankings/rankings.component.ts
+++ src/app/rankings/rankings.component.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 @Component({
 	selector: 'app-rankings',
 	templateUrl: './rankings.component.html',
-	styleUrls: ['./rankings.component.scss']
+	styleUrls: [ './rankings.component.scss' ]
 })
 export class RankingsComponent implements OnInit {
 	levelId: number;
@@ -16,7 +16,7 @@ export class RankingsComponent implements OnInit {
 	constructor(public router: Router, private route: ActivatedRoute, public http: HttpClient) {}
 
 	ngOnInit() {
-		this.route.params.subscribe(params => {
+		this.route.params.subscribe((params) => {
 			this.levelId = params.id;
 			this.difficulty = params.difficulty;
 
@@ -24,12 +24,29 @@ export class RankingsComponent implements OnInit {
 				method: 'get',
 				url: 'https://cors-anywhere.herokuapp.com/http://rankings.the-elite.net/ajax/stage/' + this.levelId,
 				headers: { Origin: 'http://rankings.the-elite.net' }
-			}).then(res => {
-				if (this.difficulty === 'agent') this.flatRuns = res.data[0];
-				else if (this.difficulty === 'secretagent') this.flatRuns = res.data[1];
-				else if (this.difficulty === '00') this.flatRuns = res.data[2];
+			}).then((res: { data: any[] }) => {
+				if (this.difficulty === 'agent') this.flatRuns = res.data[0].slice(0, 600);
+				else if (this.difficulty === 'secretagent') this.flatRuns = res.data[1].slice(0, 600);
+				else if (this.difficulty === '00') this.flatRuns = res.data[2].slice(0, 600);
 			});
 		});
+	}
+
+	fancyTimeFormat(seconds) {
+		const hrs = ~~(seconds / 3600);
+		const mins = ~~((seconds % 3600) / 60);
+		const secs = ~~seconds % 60;
+
+		// Output like "1:01" or "4:03:59" or "123:03:59"
+		let ret = '';
+
+		if (hrs > 0) {
+			ret += '' + hrs + ':' + (mins < 10 ? '0' : '');
+		}
+
+		ret += '' + mins + ':' + (secs < 10 ? '0' : '');
+		ret += '' + secs;
+		return ret;
 	}
 }
 
